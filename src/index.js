ccf0d5853bea2e8d9911e86f5da1e5c8cf4335d3
elmesheryakova <e.meshcheryakova@dairyculture.ru>
2022-01-16 20:50:37 +0300

    feat: result

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let openArr = []
+  let closeArr = []
+  let newObj = {}
+
+  bracketsConfig.map(i => {
+    openArr.push(i[0])
+    closeArr.push(i[1])
+    newObj[i[1]] = i[0]
+  })
+
+  let stack = []
+
+  for (let i = 0; i < str.length; i++) {
+    let current = str[i]
+    let topEl = stack[stack.length - 1]
+
+    if (openArr.includes(current) && closeArr.includes(current)) {
+      if (topEl === current) {
+        stack.pop()
+      } else {
+        stack.push(current)
+      }
+    } else if (openArr.includes(current)) {
+      stack.push(current)
+    } else {
+      if (stack.length === 0) {
+        return false
+      }
+
+      if (newObj[current] === topEl) {
+        stack.pop()
+      } else {
+        return false
+      }
+    }
+  }
+  return stack.length === 0
 }
